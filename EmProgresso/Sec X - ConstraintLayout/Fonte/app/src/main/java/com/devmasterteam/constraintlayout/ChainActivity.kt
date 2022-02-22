36a498981c76070e922cae18c6a719966b7875b9
Gabriel Ferrari Ceron <gferrari.ceron@outlook.com>
2022-02-22 01:19:53 -0300

    Publicação - conceitos e fundamentos Android

---
+++ EmProgresso/Sec X - ConstraintLayout/Fonte/app/src/main/java/com/devmasterteam/constraintlayout/ChainActivity.kt
@@ -0,0 +1,11 @@
+package com.devmasterteam.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chain)
+    }
+}
