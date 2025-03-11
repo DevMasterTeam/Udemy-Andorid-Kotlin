c372b494a5407da77b0746d4a3aa2d0c35209a60
DevMasterTeam <devmasterteam@gmail.com>
2025-03-10 23:48:35 -0300

    Atualização 2025. Seção 23.

---
+++ Secao 23 - LinearLayout/ConceitosElementosLinearLayout/app/src/main/java/com/devmasterteam/conceitoslinerarlayout/LinearLayoutActivity.kt
@@ -0,0 +1,20 @@
+package com.devmasterteam.conceitoslinerarlayout
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LinearLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_linear_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ Secao 23 - LinearLayout/ElementosEScrollView/app/src/main/java/com/devmasterteam/elementosescrollview/ImageActivity.kt
@@ -0,0 +1,20 @@
+package com.devmasterteam.elementosescrollview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_image)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
